1f2663e1dfc6c76558d28deef703211c6c930821
mbiarrambangalain <cosmasalain@gmail.com>
2020-09-17 06:10:18 +0100

    Add support for Arabic and Gujarati languages

--- routes/index.js
+++ routes/index.js
@@ -26,9 +26,6 @@ const appendOptions = {
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-  console.log('Headers: ' + JSON.stringify(req.headers));
-  // console.log('IP: ' + JSON.stringify(req.ip));
-
   var geo = geoip.lookup(req.ip);
   geo.range = geo.range.toString()
   geo.range = geo.range.replace(",", " - ")
@@ -36,6 +33,7 @@ router.get('/', async (req, res, next) => {
 
   var values = Object.values(geo)
   values.push(req.ip.replace("::ffff:", ""))
+  values.push("English")
 
   appendOptions.resource.values.push(values);
   appendOptions.spreadsheetId = "1I8y1TEjmqlXkOxZqn7jDBcgpMM0epFxmmyg5sP_V0js";
@@ -64,19 +62,117 @@ router.get('/', async (req, res, next) => {
       })
 });
 
-router.get('/download', async (req, res, next) => {
-  var filePath = path.join(__dirname, '../public', 'files/september2020.pdf');
-
-  res.download(filePath, async (err) => {
-    if (!err) return; // file sent
-    if (err.status !== 404) return next(err);
+/* GET arabic page. */
+router.get('/ar', async (req, res, next) => {
+    var geo = geoip.lookup(req.ip);
+    geo.range = geo.range.toString()
+    geo.range = geo.range.replace(",", " - ")
+    geo.ll = geo.ll.toString()
+  
+    var values = Object.values(geo)
+    values.push(req.ip.replace("::ffff:", ""))
+    values.push("Arabic")
+  
+    appendOptions.resource.values.push(values);
+    appendOptions.spreadsheetId = "1I8y1TEjmqlXkOxZqn7jDBcgpMM0epFxmmyg5sP_V0js";
+  
+    return await sheetClient.authorize()
+        .then(async (tokens) => {
+            const googleSheetsService = google.sheets({version: 'v4', auth: sheetClient});
+  
+            return await googleSheetsService.spreadsheets.values.append(appendOptions)
+                .then((result) => {
+                    appendOptions.resource.values = [];
+                    console.log("success")
+                    return res.sendFile(path.join(__dirname, '../views', 'arabic.html'));
+                })
+                .catch((err) => {
+                    console.log(err)
+                    appendOptions.resource.values = [];
+                    return res.sendFile(path.join(__dirname, '../views', 'arabic.html'));
+                })
+        })
+        .catch((error) => {
+            console.log(error);
+            appendOptions.resource.values = [];
+  
+            res.sendFile(path.join(__dirname, '../views', 'arabic.html'));
+        })
   });
+
+  /* GET gujarati page. */
+router.get('/gu', async (req, res, next) => {
+    var geo = geoip.lookup(req.ip);
+    geo.range = geo.range.toString()
+    geo.range = geo.range.replace(",", " - ")
+    geo.ll = geo.ll.toString()
+  
+    var values = Object.values(geo)
+    values.push(req.ip.replace("::ffff:", ""))
+    values.push("Gujarati")
+  
+    appendOptions.resource.values.push(values);
+    appendOptions.spreadsheetId = "1I8y1TEjmqlXkOxZqn7jDBcgpMM0epFxmmyg5sP_V0js";
+  
+    return await sheetClient.authorize()
+        .then(async (tokens) => {
+            const googleSheetsService = google.sheets({version: 'v4', auth: sheetClient});
+  
+            return await googleSheetsService.spreadsheets.values.append(appendOptions)
+                .then((result) => {
+                    appendOptions.resource.values = [];
+                    console.log("success")
+                    return res.sendFile(path.join(__dirname, '../views', 'gujarati.html'));
+                })
+                .catch((err) => {
+                    console.log(err)
+                    appendOptions.resource.values = [];
+                    return res.sendFile(path.join(__dirname, '../views', 'gujarati.html'));
+                })
+        })
+        .catch((error) => {
+            console.log(error);
+            appendOptions.resource.values = [];
+  
+            res.sendFile(path.join(__dirname, '../views', 'gujarati.html'));
+        })
+  });
+
+router.get('/download', async (req, res, next) => {
+    var lang = req.query.lang
+    var filePath = ""
+
+    if(lang == "arabic") {
+        filePath = path.join(__dirname, '../public', 'files/september2020arabic.pdf');
+    } else if (lang == "gujarati") {
+        filePath = path.join(__dirname, '../public', 'files/september2020gujarati.pdf');
+    } else {
+        filePath = path.join(__dirname, '../public', 'files/september2020.pdf');
+    }
+
+    res.download(filePath, async (err) => {
+        if (!err) return; // file sent
+        if (err.status !== 404) return next(err);
+    });
 });
 
 router.post('/', async (req, res, next) => {
   console.log('IP: ' + JSON.stringify(req.ip));
   console.log(req.body)
 
+  var lang = req.body.language
+  var message = ""
+  var errorMsg = ""
+  if(lang == "Arabic") {
+    message = "شكرا لك. لقد قدمت التفاصيل بنجاح. نكتب لك قريبا. بارك الله فيك."
+    errorMsg = "كانت هناك مشكلة في عملية الإرسال. حاول مرة أخرى في وقت لاحق."
+  } else if(lang == "Gujarati") {
+    message = "આભાર. તમે વિગતો સફળતાપૂર્વક સબમિટ કરી છે. અમે તમને ટૂંક સમયમાં લખીશું. ભગવાન તારુ ભલુ કરે."
+    errorMsg = "સબમિશન operationપરેશનમાં સમસ્યા આવી છે. પછીથી ફરી પ્રયાસ કરો."
+  } else {
+    message = "Thank you. You details have submitted successfully. We write to you shortly. God Bless You."
+    errorMsg = "There has been a problem with the submission operation. Try again later."
+  }
   var values = Object.values(req.body)
   values.push(req.ip.replace("::ffff:", ""))
 
@@ -91,12 +187,12 @@ router.post('/', async (req, res, next) => {
               .then((result) => {
                   appendOptions.resource.values = [];
                   console.log("success")
-                  return res.json({message: "Thank you. You details have submitted successfully. We write you shortly. God Bless You."});
+                  return res.json({message: message});
               })
               .catch((err) => {
                   console.log(err)
                   appendOptions.resource.values = [];
-                  return res.json({message: "There has been a problem with the submission operation. Try agin later."});
+                  return res.json({message: errorMsg});
               })
       })
       .catch((error) => {
