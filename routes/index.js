b791c335f2b6e377849cc5180d390844973b900c
mbiarrambangalain <cosmasalain@gmail.com>
2020-09-25 15:00:09 +0100

    Trying something else

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,11 @@ const appendOptions = {
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-    console.log(req.connection.remoteAddress)
+    var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     (req.connection.socket ? req.connection.socket.remoteAddress : null);
+    console.log(ip)
     return await request('http://ipinfo.io', { json: true }, async(err, _, body) => {
         if (err) { return console.log(err); }
         let values = Object.values(body)
