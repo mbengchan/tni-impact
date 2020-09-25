88b62d5eeb09cf1035cc609a7ca8b5974ebb8324
mbiarrambangalain <cosmasalain@gmail.com>
2020-09-25 16:44:50 +0100

    Update translation trigger

--- routes/russian.js
+++ routes/russian.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const geoip = require('geoip-lite');
+const request = require('request');
 const {google} = require('googleapis');
 const serviceAccount = require('../service-account.json');
 
@@ -24,50 +24,56 @@ const appendOptions = {
 
 };
 
-/* GET home page. */
+/* GET russian page. */
 router.get('/', async (req, res, next) => {
-  let geo = geoip.lookup(req.ip);
-  geo.range = geo.range.toString()
-  geo.range = geo.range.replace(",", " - ")
-  geo.ll = geo.ll.toString()
+    var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     (req.connection.socket ? req.connection.socket.remoteAddress : null);
+    console.log(ip)
+    return await request(`http://ipinfo.io/${ip}`, { json: true }, async(err, _, body) => {
+        if (err) { return console.log(err); }
+        let values = Object.values(body)
+        values.pop()
+        values.push("Russian")
+        console.log(values);
 
-  let values = Object.values(geo)
-  values.push(req.ip.replace("::ffff:", ""))
-  values.push("Russian")
+        appendOptions.resource.values.push(values);
+        appendOptions.spreadsheetId = "1gjjIPLFGr9CobzjnMk5d2aw-GXhFsFjFrZG6oktClwo";
 
-  appendOptions.resource.values.push(values);
-  appendOptions.spreadsheetId = "1I8y1TEjmqlXkOxZqn7jDBcgpMM0epFxmmyg5sP_V0js";
+        return await sheetClient.authorize()
+            .then(async (tokens) => {
+                const googleSheetsService = google.sheets({version: 'v4', auth: sheetClient});
 
-  return await sheetClient.authorize()
-      .then(async (tokens) => {
-          const googleSheetsService = google.sheets({version: 'v4', auth: sheetClient});
+                return await googleSheetsService.spreadsheets.values.append(appendOptions)
+                    .then((result) => {
+                        appendOptions.resource.values = [];
+                        console.log("success")
+                        return res.sendFile(path.join(__dirname, '../views', 'russian.html'));
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                        appendOptions.resource.values = [];
+                        return res.sendFile(path.join(__dirname, '../views', 'russian.html'));
+                    })
+            })
+            .catch((error) => {
+                console.log(error);
+                appendOptions.resource.values = [];
 
-          return await googleSheetsService.spreadsheets.values.append(appendOptions)
-              .then((result) => {
-                  appendOptions.resource.values = [];
-                  console.log("success")
-                  return res.sendFile(path.join(__dirname, '../views', 'russian.html'));
-              })
-              .catch((err) => {
-                  console.log(err)
-                  appendOptions.resource.values = [];
-                  return res.sendFile(path.join(__dirname, '../views', 'russian.html'));
-              })
-      })
-      .catch((error) => {
-          console.log(error);
-          appendOptions.resource.values = [];
-
-          res.sendFile(path.join(__dirname, '../views', 'russian.html'));
-      })
+                res.sendFile(path.join(__dirname, '../views', 'russian.html'));
+            })
+    });
 });
 
 router.post('/', async (req, res, next) => {
-  console.log('IP: ' + JSON.stringify(req.ip));
-  console.log(req.body)
+    var ip = req.headers['x-forwarded-for'] || 
+    req.connection.remoteAddress || 
+    req.socket.remoteAddress ||
+    (req.connection.socket ? req.connection.socket.remoteAddress : null);
 
-  var values = Object.values(req.body)
-  values.push(req.ip.replace("::ffff:", ""))
+  let values = Object.values(req.body)
+  values.push(ip.replace("::ffff:", ""))
 
   appendOptions.resource.values.push(values);
   appendOptions.spreadsheetId = "16LARNya1pWp1_IrPH_wKDhILL7HSZuKt80rscx18L8U";
