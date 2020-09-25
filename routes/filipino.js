ad702d5af48d69e99cd0b71447fb1c7c20ab20c9
mbiarrambangalain <cosmasalain@gmail.com>
2020-09-25 15:54:20 +0100

    Update trigger function

--- routes/filipino.js
+++ routes/filipino.js
@@ -26,40 +26,44 @@ const appendOptions = {
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-  let geo = geoip.lookup(req.ip);
-  geo.range = geo.range.toString()
-  geo.range = geo.range.replace(",", " - ")
-  geo.ll = geo.ll.toString()
+    var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     (req.connection.socket ? req.connection.socket.remoteAddress : null);
+    console.log(ip)
+    return await request(`http://ipinfo.io/${ip}`, { json: true }, async(err, _, body) => {
+        if (err) { return console.log(err); }
+        let values = Object.values(body)
+        values.pop()
+        values.push("Filipino")
+        console.log(values);
 
-  let values = Object.values(geo)
-  values.push(req.ip.replace("::ffff:", ""))
-  values.push("Filipino")
+        appendOptions.resource.values.push(values);
+        appendOptions.spreadsheetId = "1gjjIPLFGr9CobzjnMk5d2aw-GXhFsFjFrZG6oktClwo";
 
-  appendOptions.resource.values.push(values);
-  appendOptions.spreadsheetId = "1I8y1TEjmqlXkOxZqn7jDBcgpMM0epFxmmyg5sP_V0js";
+        return await sheetClient.authorize()
+            .then(async (tokens) => {
+                const googleSheetsService = google.sheets({version: 'v4', auth: sheetClient});
 
-  return await sheetClient.authorize()
-      .then(async (tokens) => {
-          const googleSheetsService = google.sheets({version: 'v4', auth: sheetClient});
+                return await googleSheetsService.spreadsheets.values.append(appendOptions)
+                    .then((result) => {
+                        appendOptions.resource.values = [];
+                        console.log("success")
+                        return res.sendFile(path.join(__dirname, '../views', 'filipino.html'));
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                        appendOptions.resource.values = [];
+                        return res.sendFile(path.join(__dirname, '../views', 'filipino.html'));
+                    })
+            })
+            .catch((error) => {
+                console.log(error);
+                appendOptions.resource.values = [];
 
-          return await googleSheetsService.spreadsheets.values.append(appendOptions)
-              .then((result) => {
-                  appendOptions.resource.values = [];
-                  console.log("success")
-                  return res.sendFile(path.join(__dirname, '../views', 'filipino.html'));
-              })
-              .catch((err) => {
-                  console.log(err)
-                  appendOptions.resource.values = [];
-                  return res.sendFile(path.join(__dirname, '../views', 'filipino.html'));
-              })
-      })
-      .catch((error) => {
-          console.log(error);
-          appendOptions.resource.values = [];
-
-          res.sendFile(path.join(__dirname, '../views', 'filipino.html'));
-      })
+                res.sendFile(path.join(__dirname, '../views', 'filipino.html'));
+            })
+    });
 });
 
 router.post('/', async (req, res, next) => {
